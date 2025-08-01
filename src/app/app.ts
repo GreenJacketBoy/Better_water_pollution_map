96a38908cdc95b1bef31f95b755bc2d3b220850e
Green Jacket Boy <greenjacketboy@proton.me>
2025-08-01 13:15:20 +0200

    base angular project

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'Better_water_pollution_map';
+}
